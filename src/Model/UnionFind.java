2a8470e171519e8e9fc260e0a020b03dcaf5853a
Ayanokouji2018 <martinalmada2018@gmail.com>
2026-09-23 15:01:24 -0300

    Empeza a implementar el metodo de UnionFind

--- src/Model/UnionFind.java
+++ src/Model/UnionFind.java
@@ -1,5 +1,32 @@
 package Model;
 
 public class UnionFind {
+	private int[] padre;
+	private int[] rango;
+	
+	public UnionFind(int cantidadElementos) {
+		
+	}
+	
+	public int buscar(int elemento) {
+		if (padre[elemento] != elemento) {
+			padre[elemento] = buscar(padre[elemento]);
+		}
+		return padre[elemento];
+	}
+	
+	public void unir(int elemento1, int elemento2) {
 
+	    int raiz1 = buscar(elemento1);
+	    int raiz2 = buscar(elemento2);
+
+	    if (raiz1 == raiz2) {
+	        return;
+	    }
+	    // unir según rango
+	}
+	
+	public boolean mismoConjunto(int elemento1, int elemento2) {
+		return true;
+	}
 }
