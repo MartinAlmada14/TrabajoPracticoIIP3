fe8b2a8db631cf1f3fbc6632e413bea158e0b17b
Ayanokouji2018 <martinalmada2018@gmail.com>
2026-09-24 10:54:02 -0300

    Termine de implementar UnionFind y empece a desarollar AGM

--- src/Model/ArbolGeneradorMinimo.java
+++ src/Model/ArbolGeneradorMinimo.java
@@ -1,18 +1,37 @@
 package Model;
 
+import java.util.Collections;
+import java.util.List;
+
 public class ArbolGeneradorMinimo {
 	private Grafo arbol;
 	
-	public ArbolGeneradorMinimo() {
-		
+	public ArbolGeneradorMinimo(Grafo arbol) {
+		this.arbol = arbol;
 	}
 	
-	public void eliminararistasMayores(int k) {
-		
-	}
+    public void eliminarAristasMayores(int k) {
+    	
+    	if	(k > arbol.tamano()-1) {
+			throw new IllegalArgumentException("Las posibles regiones que se pueden generar van de:" + 0 + "a" + arbol.tamano());
+    	}
+    	
+        List<Arista> aristas = arbol.aristas();
+
+        Collections.sort(aristas);
+
+        int cantidadAEliminar = k - 1;
+
+        for (int i = aristas.size()-1 ; i >= aristas.size() - cantidadAEliminar; i--) {
+
+            Arista arista = aristas.get(i);
+
+            arbol.eliminarArista(arista.getOrigen(), arista.getDestino());
+        }
+    }
 	
-	public void obtenerAristas() {
-		
+	public List<Arista> aristas() {
+		return arbol.aristas();
 	}
 	
 	public void generarRegiones() {
--- src/Model/UnionFind.java
+++ src/Model/UnionFind.java
@@ -1,32 +1,50 @@
 package Model;
 
 public class UnionFind {
-	private int[] padre;
-	private int[] rango;
-	
-	public UnionFind(int cantidadElementos) {
-		
-	}
-	
-	public int buscar(int elemento) {
-		if (padre[elemento] != elemento) {
-			padre[elemento] = buscar(padre[elemento]);
-		}
-		return padre[elemento];
-	}
-	
-	public void unir(int elemento1, int elemento2) {
 
-	    int raiz1 = buscar(elemento1);
-	    int raiz2 = buscar(elemento2);
+    private int[] padre;
+    private int[] rango;
 
-	    if (raiz1 == raiz2) {
-	        return;
-	    }
-	    // unir según rango
-	}
-	
-	public boolean mismoConjunto(int elemento1, int elemento2) {
-		return true;
-	}
+    public UnionFind(int cantidadElementos) {
+
+        padre = new int[cantidadElementos];
+        rango = new int[cantidadElementos];
+
+        for (int i = 0; i < cantidadElementos; i++) {
+            padre[i] = i;
+            rango[i] = 0;
+        }
+    }
+
+    public int buscar(int elemento) {
+        if (padre[elemento] != elemento) {
+            padre[elemento] = buscar(padre[elemento]);
+        }
+        return padre[elemento];
+    }
+
+    public void unir(int elemento1, int elemento2) {
+
+        int raiz1 = buscar(elemento1);
+        int raiz2 = buscar(elemento2);
+
+        if (raiz1 == raiz2) {
+            return;
+        }
+
+        if (rango[raiz1] < rango[raiz2]) {
+            padre[raiz1] = raiz2;
+
+        } else if (rango[raiz1] > rango[raiz2]) {
+            padre[raiz2] = raiz1;
+
+        } else {
+            padre[raiz2] = raiz1;
+            rango[raiz1]++;
+        }
+    }
+
+    public boolean mismoConjunto(int elemento1, int elemento2) {
+        return buscar(elemento1) == buscar(elemento2);
+    }
 }
